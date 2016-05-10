9ba95ca2e83befd1768731344c203501c12a41b7
Rovio01 <roviononincorporated@gmail.com>
2016-05-10 15:00:47 -0400

    Some work on Main

--- src/com/rovio/scratch_interpreter/Main.java
+++ src/com/rovio/scratch_interpreter/Main.java
@@ -1,8 +1,34 @@
 package com.rovio.scratch_interpreter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.rovio.scratch_interpreter.util.Block;
+
 public class Main {
+	
+	private List<Block> blocks = new ArrayList<Block>();
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO There really is QUITE a bit left to do here
+		// Things left to do:
+		// Get filename if not an argument
+		// Parse the file (decompress as zip, read JSON, validate syntax, 
+		//					parse into objects)
+		// Main Loop (render, run scripts, 
+		if (args.length>0) {
+			parseFile(args[0]);
+		}
+		else {
+			String filepath="temp";
+			//Get a filename with a text input
+			
+			parseFile(filepath);
+		}
+		//
+	}
+	
+	private static void parseFile(String path) {
+		
 	}
 }
